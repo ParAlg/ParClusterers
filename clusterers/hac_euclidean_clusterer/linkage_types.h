d6dc5873b60b84267178f67f0d5ee9b54bb55978
sy <shangdiy@mit.edu>
2022-04-01 15:00:27 -0400

    add node types

--- clusterers/hac_euclidean_clusterer/linkage_types.h
+++ clusterers/hac_euclidean_clusterer/linkage_types.h
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <fstream> 
 #include <math.h>
+#include <atomic>
+#include <tuple>
+#include "point.h"
+#include "parlay/utilities.h"
 
 #define SIZE_T_MAX std::numeric_limits<size_t>::max()
 #define NO_NEIGH SIZE_T_MAX
@@ -14,30 +18,56 @@ namespace in_memory {
 
 namespace internal {
 
-// consecutive points
+// Node data structure, used for dendrogram tree, each node represents a intermediate cluster
+// TODO: changed size_t to int, but need to check overflow for multication computation
+template<int dim>
 struct Node{
-    std::size_t cId;
-    std::size_t round = 0;
-    std::size_t idx; // node idx
-    std::size_t n = 1;
-    Node *left = nullptr;
-    Node *right = nullptr;
+    int cId; // cluster id corresponds to the union find id
+    int round = 0; // merged at which round. if 0, a sigleton cluster
+    int idx; // node idx, unique for each node
+    double n = 1; // use double because need to do floating point computation using n
     double height = 0;
+    Node<dim> *left = nullptr;
+    Node<dim> *right = nullptr;
+    point<dim> pMin, pMax;
+    int offset = -1;  // the offset of this node in the 2*datasize-1 arrays of all dendrogram nodes
+    point<dim> center;
 
-    Node(std::size_t  t_cid, std::size_t t_round, std::size_t t_idx, Node *t_left, Node *t_right, double _height):
+    //get the node from merged left and right nodes
+    // if height=0, assume the left and right are identical
+    Node(int t_cid, int t_round, int t_idx, Node<dim> *t_left, Node<dim> *t_right, double _height):
         cId(t_cid),
         round(t_round),
         idx(t_idx),
+        height(_height),
         left(t_left),
-        right(t_right),
-        height(_height){
+        right(t_right){
             n = t_left->n + t_right->n;
+            //compute bounding box and center
+            pMin = left->pMin;
+            pMax = left->pMax;
+            pMin.minCoords(right->pMin);
+            pMax.maxCoords(right->pMax);
+            if(height ==0){
+                center = t_left->center; //important to avoid numerical error
+            }else{
+                for (int i=0; i<dim; ++i) {
+                    center.updateX(i, 
+                                    (t_left->n    / n * t_left->center[i] ) + 
+                                    (t_right->n   / n * t_right->center[i]) );
+                }
+            }
         }
     
-    Node(std::size_t  t_cid):
+    // initialize a singleton node for a point
+    Node(std::size_t  t_cid, point<dim> p):
         cId(t_cid),
-        idx(t_cid){
-        }
+        idx(t_cid),
+        offset(t_cid){
+                pMin = p;
+                pMax = p;
+                center = p;
+            }
     Node(){}
 
     inline bool isLeaf() const {return n == 1;}
@@ -45,19 +75,59 @@ struct Node{
     inline std::size_t getRound() const {return round;}
     inline std::size_t getIdx() const {return idx;}
     inline std::size_t size() const {return n;}
+    inline int getOffset(){return offset;}
+    inline void setOffset(int i){offset = i;}
+    inline double dist(Node<dim> *node){return center.pointDistSq(node->center);}
 };
 
+// used for dendrogram sorting 
+template<int dim>
 struct nodeComparator{
   double eps;
   nodeComparator(double _eps):eps(_eps) {
   }
 
-  bool operator() (const Node i, const Node j) const {
+  bool operator() (const Node<dim> i, const Node<dim> j) const {
       if(abs(i.getHeight() - j.getHeight()) <= eps) return i.getRound() < j.getRound();
      return i.getHeight() < j.getHeight();
   }
 };
 
+
+struct kdNodeInfo{
+    typedef std::tuple<int, int> infoT; // <cid, min_n>
+    int cId;
+    std::atomic<double> ub;
+    int round = 0;
+    int idx = -1; // node idx
+    int min_n = 1; //used for ward's linkage
+
+    kdNodeInfo(){
+        cId = -1;
+        ub.store(numeric_limits<double>::max()); // used for allptsnn
+    }
+
+    inline double getUB(){return ub.load();}
+    inline void updateUB(double tmp){
+        parlay::write_min(&ub, tmp, std::less<double>());
+    }
+    inline void resetUB(){
+        ub.store(numeric_limits<double>::max());
+    }
+    inline int getCId(){return cId;}
+    inline void setCId(int id){cId = id;}
+    inline int getRound(){return round;}
+    inline void setRound(int r){round = r;}
+    inline int getIdx(){return idx;}
+    inline void setIdx(int r){idx = r;}
+    inline infoT initInfoVal() {return make_tuple(-1, numeric_limits<int>::max());}
+    inline void setInfo(infoT info){
+      tie(cId, min_n) = info;
+    }
+    inline double getMinN(){return (double)min_n;} //return double because it's used in floating point computation
+    inline void setMinN(int nn){min_n = nn;}
+
+};
 
 struct EDGE{//nodes unordered
     volatile std::size_t first;
---
+++ clusterers/hac_euclidean_clusterer/point.h
@@ -0,0 +1,192 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+
+namespace research_graph {
+namespace in_memory {
+
+// from pbbs
+template <int _dim> class point;
+
+template <int _dim> class vect {
+public:
+  typedef double floatT;
+  typedef vect vectT;
+  typedef point<_dim> pointT;
+  floatT x[_dim];
+  static const int dim = _dim;
+  vect() { for (int i=0; i<_dim; ++i) x[i] = 0; }
+  vect(pointT p) { for (int i=0; i<_dim; ++i) x[i] = p.x[i]; }
+  vect(floatT* v) { for (int i=0; i<_dim; ++i) x[i] = v[i]; }
+  void print() {
+    cout << std::setprecision(2);
+    cout << ":(";
+    for (int i=0; i<_dim-1; ++i) cout << x[i] << ",";
+    cout << x[_dim-1] << "):";
+  }
+  vectT operator+(vectT op2) {
+    floatT xx[_dim];
+    for (int i=0; i<_dim; ++i) xx[i]=x[i] + op2.x[i];
+    return vectT(xx);
+  }
+  vectT operator-(vectT op2) {
+    floatT xx[_dim];
+    for (int i=0; i<_dim; ++i) xx[i]=x[i] - op2.x[i];
+    return vectT(xx);
+  }
+  pointT operator+(pointT op2);
+  vectT operator*(floatT s) {
+    floatT xx[_dim];
+    for (int i=0; i<_dim; ++i) xx[i] = x[i]*s;
+    return vectT(xx);
+  }
+  vectT operator/(floatT s) {
+    floatT xx[_dim];
+    for (int i=0; i<_dim; ++i) xx[i] = x[i]/s;
+    return vectT(xx);
+  }
+  floatT& operator[] (int i) {return x[i];}
+  float dot(vectT v) {
+    floatT xx=0;
+    for (int i=0; i<_dim; ++i) xx += x[i]*v[i];
+    return xx;
+  }
+  vectT cross(vectT v) {
+    cout << "vect cross not implemented, abort" << endl; abort();
+  }
+  floatT maxDim() {
+    floatT xx = x[0];
+    for (int i=1; i<_dim; ++i) xx = max(xx,x[i]);
+    return xx;
+  }
+  floatT length() {
+    floatT xx=0;
+    for (int i=0; i<_dim; ++i) xx += x[i]*x[i];
+    return sqrt(xx);
+  }
+};
+
+template <int _dim> class point {
+public:
+  typedef double floatT;
+  typedef vect<_dim> vectT;
+  typedef point pointT;
+  floatT x[_dim];
+  static const int dim = _dim;
+  static constexpr double empty = numeric_limits<double>::max();
+  int dimension() {return _dim;}
+  bool isEmpty() {return x[0]==empty;}
+  point() { for (int i=0; i<_dim; ++i) x[i]=empty; }
+  point(vectT v) { for (int i=0; i<_dim; ++i) x[i]=v[i]; }
+  point(floatT* p) { for (int i=0; i<_dim; ++i) x[i]=p[i]; }
+  point(pointT* p) { for (int i=0; i<_dim; ++i) x[i]=p->x[i]; }
+//   void print() {
+//     cout << std::setprecision(2);
+//     cout << ":(";
+//     for (int i=0; i<_dim-1; ++i) cout << x[i] << ",";
+//     cout << x[_dim-1] << "):";
+//   }
+  pointT operator+(vectT op2) {
+    floatT xx[_dim];
+    for (int i=0; i<_dim; ++i) xx[i] = x[i]+op2.x[i];
+    return pointT(xx);
+  }
+  vectT operator-(pointT op2) {
+    floatT xx[_dim];
+    for (int i=0; i<_dim; ++i) xx[i] = x[i]-op2.x[i];
+    return pointT(xx);
+  }
+  floatT operator[](int i) {return x[i];}
+  void updateX(int i, floatT val) {x[i]=val;}
+  void minCoords(pointT b) {
+    for (int i=0; i<_dim; ++i) x[i] = min(x[i], b.x[i]);}
+  void minCoords(floatT* b) {
+    for (int i=0; i<_dim; ++i) x[i] = min(x[i], b[i]);}
+  void maxCoords(pointT b) {
+    for (int i=0; i<_dim; ++i) x[i] = max(x[i], b.x[i]);}
+  void maxCoords(floatT* b) {
+    for (int i=0; i<_dim; ++i) x[i] = max(x[i], b[i]);}
+//   intT quadrant(pointT center) {
+//     intT index = 0;
+//     intT offset = 1;
+//     for (int i=0; i<_dim; ++i) {
+//       if (x[i] > center.x[i]) index += offset;
+//       offset *= 2;
+//     }
+//     return index;
+//   }
+  bool outOfBox(pointT center, floatT hsize) {
+    for (int i=0; i<_dim; ++i) {
+      if (x[i]-hsize > center.x[i] || x[i]+hsize < center.x[i])
+        return true;
+    }
+    return false;
+  }
+  floatT pointDist(pointT p) {
+    floatT xx=0;
+    for (int i=0; i<_dim; ++i) xx += (x[i]-p.x[i])*(x[i]-p.x[i]);
+    return sqrt(xx);
+  }
+  
+  floatT pointDistSq(point p) {
+    floatT xx=0;
+    for (int i=0; i<_dim; ++i) xx += (x[i]-p.x[i])*(x[i]-p.x[i]);
+    return xx;
+  }
+};
+
+template<int dim>
+struct iPoint {
+  typedef double floatT;
+  typedef point<dim> pointT;
+  int i;
+  pointT p;
+  iPoint(pointT pp, int ii): p(pp), i(ii) {}
+  iPoint(pointT pp): p(pp), i(-1) {}
+  iPoint(): i(-1) {}
+  bool isEmpty() {return i<0;}
+  floatT operator[](int i) {return p[i];}
+  floatT pointDist(iPoint q) {return p.pointDist(q.p);}
+  floatT pointDist(iPoint *q) {return p.pointDist(q->p);}
+  floatT dist(iPoint q) {return p.pointDist(q.p);}
+  floatT pointDist(pointT q) {return p.pointDist(q);}
+  floatT pointDistSq(iPoint q) {return p.pointDistSq(q.p);}
+  floatT pointDistSq(iPoint *q) {return p.pointDistSq(q->p);}
+  floatT pointDistSq(pointT q) {return p.pointDistSq(q);}
+  int idx() {return i;}
+  void idx(int ii) {i=ii;}
+  pointT pt() {return p;}
+  floatT* coordinate() {return p.x;}
+  floatT coordinate(int i) {return p.x[i];}
+  floatT x(int i) {return p.x[i];}
+  void x(int i, floatT val) {p.x[i]=val;}
+  int getDim(){return dim;}
+  void minCoords(iPoint q){p.minCoords(q.p);}
+  void maxCoords(iPoint q){p.maxCoords(q.p);}
+  void print(){p.print();}
+};
+
+template<int dim>
+static std::ostream& operator<<(std::ostream& os, const point<dim> v) {
+  for (int i=0; i<v.dim; ++i)
+    os << std::setprecision(2) << v.x[i] << ", ";
+  return os;
+}
+
+template<int dim>
+static std::ostream& operator<<(std::ostream& os, const iPoint<dim> v) {
+  for (int i=0; i<v.dim; ++i)
+    os << std::setprecision(2) << v.x[i] << ", ";
+  return os;
+}
+
+
+}
+}
