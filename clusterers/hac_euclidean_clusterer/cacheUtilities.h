0c9eb26db08bef062e4e60dfa624b2bd42767638
sy <shangdiy@mit.edu>
2022-04-12 12:48:50 -0400

    cleanup cache and update distances

--- clusterers/hac_euclidean_clusterer/cacheUtilities.h
+++ clusterers/hac_euclidean_clusterer/cacheUtilities.h
@@ -24,20 +24,6 @@ namespace HACTree {
 #define CHECK_TOKEN -1
 #define UNFOUND_TOKEN -2
 
-//   struct hashClusterId { 
-//     typedef pair<int,int> eType;
-//     typedef int kType;
-//     eType empty() {return pair<int,int>(-1,-1);}
-//     kType getKey(eType v) { return v.first; }
-//     std::size_t hash(kType v) { return static_cast<std::size_t>(parlay::hash32((std::uint32_t)v)); }
-//     int cmp(int v, int b) {return (v > b) ? 1 : ((v == b) ? 0 : -1);}
-//     bool replaceQ(eType s, eType s2) {cout << "wrong replace Q" << endl;
-//         exit(1);return 0;}//return s.second > s2.second;}
-//     bool replaceQ(eType s, eType* TA, std::size_t h) {
-//         utils::writeAdd(&(TA[h].second), s.second);
-//     return false;}
-//   };
-
   struct hashClusterET {
     volatile int first;
     volatile int idx;
@@ -77,7 +63,8 @@ namespace HACTree {
       }//return s.second > s2.second;}
   };
 
-// TODO: consider changing hashtable to idx -> (idx, dist)
+// TODO: consider changing hashtable to idx -> dist
+// if cmx16 does not work, need change all dist type from double to float in addition, and change the edge type to (int, float)
 template<class nodeT>
 struct CacheTables{
     typedef int intT;
@@ -99,7 +86,7 @@ struct CacheTables{
         if(!no_cache){ //TODO: double check
             // hashTableSize = min(n, (1 << parlay::log2_up((uint)CACHE_TABLE_SIZE)));
             hashTableSize = min(n, CACHE_TABLE_SIZE);
-            TA = (distCacheT::eType *) malloc(sizeof(distCacheT::eType)* ((std::size_t)n*2*hashTableSize));
+            TA = (distCacheT::eType *) aligned_alloc(16, sizeof(distCacheT::eType)* ((std::size_t)n*2*hashTableSize));
             distCacheT::eType emptyval = hashCluster().empty();
             cacheTbs = (distCacheT **) malloc(sizeof(distCacheT *)*2*n);
             parlay::parallel_for(0,n,[&](std::size_t i){
@@ -161,22 +148,10 @@ struct CacheTables{
 	  return find(cid(inode), cid(jnode), idx(inode), idx(jnode));
   }
 
-    inline void insert_helper(intT qid, intT rid, double d, distCacheT *tb1, distCacheT *tb2){
-        if(d == LARGER_THAN_UB){
-            return;
-        }
-        CHECK_NO_CACHE(284)
-        intT qIdx = idx(qid);
-        intT rIdx = idx(rid);
-        tb1->insert_one_update(hashClusterET(rid, rIdx, d));
-        tb2->insert_one_update(hashClusterET(qid, qIdx, d));
-  }
-
     inline void insert(intT qid, intT rid, double d){
         if(d == LARGER_THAN_UB){
             return;
         }
-        CHECK_NO_CACHE(291)
         intT qIdx = idx(qid);
         intT rIdx = idx(rid);
 
@@ -184,14 +159,12 @@ struct CacheTables{
         cacheTbs[rIdx]->insert_one_update(hashClusterET(qid, qIdx, d));
   }
 
-     // return true only when insert if sucussful
-    // neex the  do Swap
-    // if one is old and one is new, always check tb[new]->old
-    // because the one stored in old might be outdated
-    // must have enough space due to alloc new
-    // calculate distance if return true
+  // return true & iffull only when insert if sucussful
+  // if one is old and one is new, always check tb[new]->old
+  // because the one stored in old might be outdated
+  // calculate distance if return true
+  // TODO: can we make doSwap=true for all use cases?
   inline bool insert_check(intT qid, intT rid, bool doSwap, bool iffull){
-      CHECK_NO_CACHE(473)
       if(doSwap && qid > rid){
           swap(qid, rid);
       }
