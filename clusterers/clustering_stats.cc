61093c87565e1092f025788f4c50273a418a3a0a
Jessica Shi <jessicashi@gmail.com>
2022-06-21 23:09:11 -0400

    add anyproto processing + community stats

--- clusterers/clustering_stats.cc
+++ clusterers/clustering_stats.cc
@@ -6,11 +6,12 @@ ClusteringStatistics GetStats(const GbbsGraph& graph,
   const InMemoryClusterer::Clustering& clustering,
   const std::string& input_graph, const std::string& input_communities,
   const ClusteringStatsConfig& clustering_stats_config) {
-    clustering_stats_.set_filename(input_graph);
-    
+    ClusteringStatistics clustering_stats;
+    clustering_stats.set_filename(input_graph);
+
     if (!input_communities.empty()) CompareCommunities(input_communities.c_str(), clustering);
 
-    return clustering_stats_;
+    return clustering_stats;
   }
 
 }  // namespace research_graph::in_memory
