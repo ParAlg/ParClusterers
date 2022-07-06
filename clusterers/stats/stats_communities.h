cb5e39645b0cdffd634dd3bb540a486d1acdfc3b
Jessica Shi <jessicashi@gmail.com>
2022-07-06 01:05:50 -0400

    fix uintE

--- clusterers/stats/stats_communities.h
+++ clusterers/stats/stats_communities.h
@@ -78,12 +78,14 @@ inline absl::Status CompareCommunities(const char* filename, const InMemoryClust
   auto precision_func = [&](std::size_t i) {
     return precision_vec[i];
   };
-  set_aggregate_statistics(precision_vec.size(), precision_func, clustering_stats->mutable_community_precision());
+  auto prec = clustering_stats->mutable_community_precision();
+  set_aggregate_statistics(precision_vec.size(), precision_func, prec);
 
   auto recall_func = [&](std::size_t i) {
     return recall_vec[i];
   };
-  set_aggregate_statistics(recall_vec.size(), recall_func, clustering_stats->mutable_community_recall());
+  auto rec = clustering_stats->mutable_community_recall();
+  set_aggregate_statistics(recall_vec.size(), recall_func, rec);
 
   /*double avg_precision = parlay::reduce(precision_vec);
   double avg_recall = parlay::reduce(recall_vec);
