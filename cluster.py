f4bd33413dd090a610615912cd93e59383ea74ac
Jessica Shi <jessicashi@gmail.com>
2022-07-26 16:43:26 -0400

    fix some formatting

--- cluster.py
+++ cluster.py
@@ -33,7 +33,7 @@ def makeConfigCombos(current_configs):
 
 def readConfig(filename):
   global input_directory, output_directory, clusterers, graphs, num_threads, clusterer_configs, num_rounds, timeout, clusterer_config_names, gbbs_format
-  input_directory, output_directory, graphs, num_threads, clusterer_configs, num_rounds, timeout, clusterer_config_names, gbbs_format = None
+  num_threads = num_rounds = timeout = gbbs_format = None
   clusterers = []
   with open(filename, "r") as in_file:
     for line in in_file:
