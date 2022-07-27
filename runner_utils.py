88a26873a90c9c8fec36c6c14c48fa6a356be58a
Jessica Shi <jessicashi@gmail.com>
2022-07-27 07:28:00 -0400

    refactor + add stats

---
+++ runner_utils.py
@@ -0,0 +1,104 @@
+import os
+import sys
+import signal
+import time
+import subprocess
+import re
+import itertools
+
+def signal_handler(signal,frame):
+  print "bye\n"
+  sys.exit(0)
+signal.signal(signal.SIGINT,signal_handler)
+
+def shellGetOutput(str1) :
+  process = subprocess.Popen(str1,shell=True,stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+  output, err = process.communicate()
+  
+  if (len(err) > 0):
+    print(str1+"\n"+output+err)
+  return output
+
+def appendToFile(out, filename):
+  with open(filename, "a+") as out_file:
+    out_file.writelines(out)
+
+def makeConfigCombos(current_configs):
+  config_combos = itertools.product(*current_configs)
+  config_combos_formatted = []
+  for config in config_combos:
+    config_combos_formatted.append(",".join(config))
+  return config_combos_formatted
+
+def readConfig(filename):
+  global input_directory, output_directory, clusterers, graphs, num_threads
+  global clusterer_configs, num_rounds, timeout, clusterer_config_names
+  global gbbs_format
+  num_threads = num_rounds = timeout = gbbs_format = None
+  clusterers = []
+  with open(filename, "r") as in_file:
+    for line in in_file:
+      line = line.strip()
+      split = [x.strip() for x in line.split(':')]
+      if split:
+        if split[0].startswith("Input directory"):
+          input_directory = split[1]
+        elif split[0].startswith("Output directory"):
+          output_directory = split[1]
+        elif split[0].startswith("Clusterers"):
+          clusterers = [x.strip() for x in split[1].split(';')]
+          clusterer_configs = len(clusterers)*[None]
+          clusterer_config_names = len(clusterers)*[None]
+        elif split[0].startswith("Graphs"):
+          graphs = [x.strip() for x in split[1].split(';')]
+        elif split[0].startswith("Number of threads") and len(split) > 1:
+          num_threads = [x.strip() for x in split[1].split(';')]
+        elif split[0].startswith("Number of rounds") and len(split) > 1:
+          num_rounds = 1 if split[1] == "" else int(split[1])
+        elif split[0].startswith("Timeout") and len(split) > 1:
+          timeout = split[1]
+        elif split[0].startswith("GBBS format") and len(split) > 1:
+          gbbs_format = split[1]
+        else:
+          for index, clusterer_name in enumerate(clusterers):
+            if split[0].startswith(clusterer_name):
+              clusterer_config_names[index] = in_file.next().strip()
+              current_configs = []
+              next_line = in_file.next().strip()
+              while next_line != "":
+                arg_name = next_line.split(':', 1)
+                arg_name[0] = arg_name[0].strip()
+                args = [x.strip() for x in arg_name[1].split(';')]
+                current_configs.append([arg_name[0] + ": " + x for x in args])
+                try:
+                  next_line = in_file.next().strip()
+                except StopIteration as err:
+                  break
+              clusterer_configs[index] = makeConfigCombos(current_configs)
+              break
+  num_threads = ["ALL"] if num_threads is None or not num_threads else num_threads
+  timeout = "" if (timeout is None or timeout == "" or timeout == "NONE") else "timeout " + timeout
+  num_rounds = 1 if (num_rounds is None) else num_rounds
+  gbbs_format = "false" if (gbbs_format is None or gbbs_format == "") else gbbs_format
+
+  def readStatsConfig(filename):
+    global communities, stats_config
+    communities = []
+    stats_config_list = []
+    with open(filename, "r") as in_file:
+    for line in in_file:
+      line = line.strip()
+      split = [x.strip() for x in line.split(':')]
+      if split:
+        if split[0].startswith("Input communities") and len(split) > 1:
+          communities = [x.strip() for x in split[1].split(';')]
+        elif split[0].startswith("statistics_config"):
+          next_line = in_file.next().strip()
+          while next_line != "":
+            stats_config_list.append(next_line)
+            try:
+              next_line = in_file.next().strip()
+            except StopIteration as err:
+              break
+          stats_config = ",".join(stats_config_list)
--- stats.py
+++ stats.py
@@ -7,8 +7,9 @@ import re
 import itertools
 import runner_utils
 
-def runAll(config_filename):
+def runAll(config_filename, stats_config_filename):
   runner_utils.readConfig(config_filename)
+  runner_utils.readStatsConfig(stats_config_filename)
   for clusterer_idx, clusterer in enumerate(runner_utils.clusterers):
     for graph_idx, graph in enumerate(runner_utils.graphs):
       for thread_idx, thread in enumerate(runner_utils.num_threads):
@@ -17,18 +18,20 @@ def runAll(config_filename):
         config_postfix = "}" if runner_utils.clusterer_configs[clusterer_idx] is not None else ""
         for config_idx, config in enumerate(configs):
           for i in range(runner_utils.num_rounds):
-            out_prefix = runner_utils.output_directory + clusterer + "_" + str(graph_idx) + "_" + thread + "_" + str(config_idx) + "_" + str(i)
-            out_filename = out_prefix + ".out"
-            out_clustering = out_prefix + ".cluster"
-            use_thread = "" if (thread == "" or thread == "ALL") else "PARLAY_NUM_THREADS=" + thread
+            out_prefix = runner_utils.output_directory + runner_utils.clusterer + "_" + str(graph_idx) + "_" + thread + "_" + str(config_idx) + "_" + str(i)
+            out_statistics = out_prefix + ".stats"
+            in_clustering = out_prefix + ".cluster"
             use_input_graph = runner_utils.input_directory + runner_utils.graph
-            ss = (use_thread + " " + runner_utils.timeout + " bazel run //clusterers:cluster-in-memory_main -- --"
-            "input_graph=" + use_input_graph + " --is_gbbs_format=" + runner_utils.gbbs_format + " --clusterer_name=" + runner_utils.clusterer + " "
-            "--clusterer_config='" + config_prefix + config + config_postfix + "' "
-            "--output_clustering=" + out_clustering)
+            use_input_communities = "" if not runner_utils.communities else "--input_communities=" + runner_utils.input_communities[graph_idx]
+            ss = ("bazel run //clusterers:stats-in-memory_main -- "
+            "--input_graph=" + use_input_graph + " "
+            "--is_gbbs_format=" + runner_utils.gbbs_format + " "
+            "--input_clustering=" + in_clustering + " "
+            "--output_statistics=" + out_statistics + " " + use_input_communities + " "
+            "--statistics_config='{" + stats_config + "}'")
             out = runner_utils.shellGetOutput(ss)
-            runner_utils.appendToFile(ss + "\n", out_filename)
-            runner_utils.appendToFile(out, out_filename)
+            # runner_utils.appendToFile(ss + "\n", out_filename)
+            # runner_utils.appendToFile(out, out_filename)
 
 def main():
   args = sys.argv[1:]
