5bffd5c7db4c0225cc421d87e64d3dc820c7d735
sy <shangdiy@mit.edu>
2022-08-31 12:08:32 -0400

    cleanup

--- clusterers/stats/stats_utils.h
+++ clusterers/stats/stats_utils.h
@@ -50,13 +50,13 @@ inline void set_distribution_stats(std::size_t size, const T& data, Distribution
 }
 
 // return a subgraph that has the vertices in V
-// the ids in subgraph do not neccessarily corresponds to the original graph's node id
-// the number of nodes in the new subgraph = V.size()
+// if keep_ids=false, the ids in subgraph do not neccessarily corresponds to the original graph's node id
+// the number of nodes in the new subgraph is V.size()
 // labels[i] is the cluster id of vertex i
-inline gbbs::symmetric_graph<gbbs::symmetric_vertex, float> get_subgraph(const GbbsGraph& graph_, const std::vector<InMemoryClusterer::NodeId>& V, const parlay::sequence<gbbs::uintE>& labels, bool keep_ids = false){
-    using Wgh = float;
+template <class Wgh=float>
+inline gbbs::symmetric_graph<gbbs::symmetric_vertex, Wgh> get_subgraph(const GbbsGraph& graph_, const std::vector<InMemoryClusterer::NodeId>& V, const parlay::sequence<gbbs::uintE>& labels, bool keep_ids = false){
+    // using Wgh = float;
     using uintE = gbbs::uintE;
-    // auto G = graph_.Graph();
     std::size_t n = V.size();
     auto offsets = parlay::sequence<size_t>::from_function(
       n, [&](size_t i) { return graph_.Graph()->get_vertex(V[i]).out_degree(); }); //double check, is numbers in V corresponding to the graph nodes?
@@ -84,52 +84,6 @@ inline gbbs::symmetric_graph<gbbs::symmetric_vertex, float> get_subgraph(const G
             if(labels[u]==labels[v]){
                 flags[offset+j] = true;
                 edges[offset+j] = std::tuple<uintE, uintE, Wgh>(map[u],map[v],wgh);
-            }
-        };
-        graph_.Graph()->get_vertex(vert).out_neighbors().map_with_index(map_f);
-    });
-
-    auto subgraph_edges = parlay::pack(edges, flags);
-
-    if(keep_ids){
-        return gbbs::sym_graph_from_edges(subgraph_edges, graph_.Graph()->n);
-    }else{
-        return gbbs::sym_graph_from_edges(subgraph_edges, n);
-    }
-
-}
-
-inline gbbs::symmetric_graph<gbbs::symmetric_vertex, gbbs::empty> get_subgraph_unweighted(const GbbsGraph& graph_, const std::vector<InMemoryClusterer::NodeId>& V, const std::vector<int>& labels, bool keep_ids = false){
-    using Wgh = gbbs::empty;
-    using uintE = gbbs::uintE;
-    // auto G = graph_.Graph();
-    std::size_t n = V.size();
-    auto offsets = parlay::sequence<size_t>::from_function(
-      n, [&](size_t i) { return graph_.Graph()->get_vertex(V[i]).out_degree(); }); //double check, is numbers in V corresponding to the graph nodes?
-    auto new_m = parlay::scan_inclusive_inplace(offsets);
-    auto edges = parlay::sequence<std::tuple<uintE, uintE, Wgh>>::uninitialized(new_m);
-    auto flags = parlay::sequence<bool>(new_m, false); // flag[i] = true if the edge should be in the subgraph
-
-    auto map =  parlay::sequence<InMemoryClusterer::NodeId>::uninitialized(graph_.Graph()->n); // map from original graph id to new id in subgraph
-    if(keep_ids){
-    parlay::parallel_for(0, n, [&] (size_t i) {
-        map[V[i]] = V[i];
-    });
-    }else{
-    parlay::parallel_for(0, n, [&] (size_t i) {
-        map[V[i]] = i;
-    });
-    }
-
-
-    // copy edges in the subgraph to edges, and mark true in flags
-    parlay::parallel_for(0, n, [&] (size_t i) {
-        uintE vert = V[i];
-        std::size_t offset = i==0 ? 0 : offsets[i-1];
-        auto map_f = [&] (const auto& u, const auto& v, const auto& wgh, const auto& j) {
-            if(labels[u]==labels[v]){
-                flags[offset+j] = true;
-                edges[offset+j] = std::tuple<uintE, uintE, Wgh>(map[u],map[v]);
             }
         };
         graph_.Graph()->get_vertex(vert).out_neighbors().map_with_index(map_f);
