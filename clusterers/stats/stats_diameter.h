17c49c3a564105586c6d9109846599a5c10b4b9b
Shangdi Yu <sy543@cornell.edu>
2022-09-12 16:34:57 -0400

    add diameter stats
    * add diameter stats

---
+++ clusterers/stats/stats_diameter.h
@@ -0,0 +1,93 @@
+#pragma once
+#ifndef RESEARCH_GRAPH_IN_MEMORY_CLUSTERING_STATS_DIAMETER_H_
+#define RESEARCH_GRAPH_IN_MEMORY_CLUSTERING_STATS_DIAMETER_H_
+
+#include <chrono>
+#include <iomanip>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_format.h"
+#include "absl/strings/string_view.h"
+
+#include "google/protobuf/text_format.h"
+#include "google/protobuf/repeated_field.h"
+#include "clusterers/clustering_stats.pb.h"
+#include "clusterers/stats/stats_utils.h"
+#include "parcluster/api/gbbs-graph.h"
+#include "parcluster/api/in-memory-clusterer-base.h"
+#include "parcluster/api/status_macros.h"
+
+#include "external/gbbs/benchmarks/GeneralWeightSSSP/BellmanFord/BellmanFord.h"
+
+namespace research_graph::in_memory {
+
+// Same as the gbbs BellmanFord algorithm, but no print statement
+template <class Graph>
+auto BellmanFordNoPrint(Graph& G, gbbs::uintE start) {
+  using W = typename Graph::weight_type;
+  using Distance =
+      typename std::conditional<std::is_same<W, gbbs::empty>::value, gbbs::uintE,
+                                W>::type;
+
+  size_t n = G.n;
+  auto Visited = parlay::sequence<int>(n, 0);
+  auto SP = parlay::sequence<Distance>(n, std::numeric_limits<Distance>::max());
+  SP[start] = 0;
+
+  gbbs::vertexSubset Frontier(n, start);
+  size_t round = 0;
+  while (!Frontier.isEmpty()) {
+    // Check for a negative weight cycle
+    if (round == n) {
+      std::cout << " Found negative weight cycle." << std::endl;
+      break;
+    }
+    auto em_f = gbbs::BF_F<W, Distance>(SP.begin(), Visited.begin());
+    auto output =
+        gbbs::edgeMap(G, Frontier, em_f, G.m / 10, gbbs::sparse_blocked | gbbs::dense_forward);
+    gbbs::vertexMap(output, gbbs::BF_Vertex_F(Visited.begin()));
+    Frontier = std::move(output);
+    round++;
+  }
+  return SP;
+}
+
+inline absl::Status ComputeDiameter(const GbbsGraph& graph, 
+  const InMemoryClusterer::Clustering& clustering, ClusteringStatistics* clustering_stats,
+  const parlay::sequence<gbbs::uintE>& cluster_ids, const ClusteringStatsConfig& clustering_stats_config) {
+
+  const bool compute_diameter = clustering_stats_config.compute_diameter();
+  
+  if (!compute_diameter) {
+    return absl::OkStatus();
+  }
+  parlay::sequence<double> diameter_vec = parlay::sequence<double>::uninitialized(clustering.size());
+
+  parlay::parallel_for(0, clustering.size(), [&] (size_t i) {
+      auto G = get_subgraph(graph, clustering[i], cluster_ids); 
+      auto distances = parlay::sequence<float>::uninitialized(clustering[i].size());
+      parlay::parallel_for(0, clustering[i].size(), [&] (size_t j) {
+          auto SP = BellmanFordNoPrint(G, j); //use j because the subgraph is re-indexed
+          distances[j] = *(parlay::max_element(SP));            
+      });
+      auto diameter = *(parlay::max_element(distances));
+      diameter_vec[i] = diameter;
+  });
+
+  auto diameter_func = [&](std::size_t i) {
+    return diameter_vec[i];
+  };
+  set_distribution_stats(diameter_vec.size(), diameter_func, clustering_stats->mutable_diameter());
+
+  return absl::OkStatus();
+}
+
+}  // namespace research_graph::in_memory
+
+#endif  // RESEARCH_GRAPH_IN_MEMORY_CLUSTERING_STATS_DIAMETER_H_
--- clusterers/stats/stats_utils.h
+++ clusterers/stats/stats_utils.h
@@ -35,8 +35,8 @@ inline void split(const std::string& s, char delim, std::vector<InMemoryClustere
 template <class T>
 inline void set_distribution_stats(std::size_t size, const T& data, DistributionStats* distribution_stats){
   double sum = 0;
-  gbbs::uintE min = UINT_E_MAX;
-  gbbs::uintE max = 0;
+  double min = std::numeric_limits<double>::max();
+  double max = std::numeric_limits<double>::lowest();
   for (std::size_t i = 0; i < size; i++) {
     sum += data(i);
     if (data(i) < min) min = data(i);
@@ -47,6 +47,56 @@ inline void set_distribution_stats(std::size_t size, const T& data, Distribution
   distribution_stats->set_mean(sum / size);
   distribution_stats->set_minimum(min);
   distribution_stats->set_maximum(max);
+}
+
+// return a subgraph that has the vertices in V
+// if keep_ids=false, the ids in subgraph do not neccessarily corresponds to the original graph's node id
+// the number of nodes in the new subgraph is V.size()
+// labels[i] is the cluster id of vertex i
+template <class Wgh=float>
+inline gbbs::symmetric_graph<gbbs::symmetric_vertex, Wgh> get_subgraph(const GbbsGraph& graph_, const std::vector<InMemoryClusterer::NodeId>& V, const parlay::sequence<gbbs::uintE>& labels, bool keep_ids = false){
+    // using Wgh = float;
+    using uintE = gbbs::uintE;
+    std::size_t n = V.size();
+    auto offsets = parlay::sequence<size_t>::from_function(
+      n, [&](size_t i) { return graph_.Graph()->get_vertex(V[i]).out_degree(); }); //double check, is numbers in V corresponding to the graph nodes?
+    auto new_m = parlay::scan_inclusive_inplace(offsets);
+    auto edges = parlay::sequence<std::tuple<uintE, uintE, Wgh>>::uninitialized(new_m);
+    auto flags = parlay::sequence<bool>(new_m, false); // flag[i] = true if the edge should be in the subgraph
+
+    auto map =  parlay::sequence<InMemoryClusterer::NodeId>::uninitialized(graph_.Graph()->n); // map from original graph id to new id in subgraph
+    if(keep_ids){
+    parlay::parallel_for(0, n, [&] (size_t i) {
+        map[V[i]] = V[i];
+    });
+    }else{
+    parlay::parallel_for(0, n, [&] (size_t i) {
+        map[V[i]] = i;
+    });
+    }
+
+
+    // copy edges in the subgraph to edges, and mark true in flags
+    parlay::parallel_for(0, n, [&] (size_t i) {
+        uintE vert = V[i];
+        std::size_t offset = i==0 ? 0 : offsets[i-1];
+        auto map_f = [&] (const auto& u, const auto& v, const auto& wgh, const auto& j) {
+            if(labels[u]==labels[v]){
+                flags[offset+j] = true;
+                edges[offset+j] = std::tuple<uintE, uintE, Wgh>(map[u],map[v],wgh);
+            }
+        };
+        graph_.Graph()->get_vertex(vert).out_neighbors().map_with_index(map_f);
+    });
+
+    auto subgraph_edges = parlay::pack(edges, flags);
+
+    if(keep_ids){
+        return gbbs::sym_graph_from_edges(subgraph_edges, graph_.Graph()->n);
+    }else{
+        return gbbs::sym_graph_from_edges(subgraph_edges, n);
+    }
+
 }
 
 }  // namespace research_graph::in_memory
