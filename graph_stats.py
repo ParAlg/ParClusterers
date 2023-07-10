5510c450e6c1fb6e1670057df1a152879877db7f
jamisonmeindl <meindljamie@gmail.com>
2023-07-10 14:45:29 +0000

    added graphing options

--- graph_stats.py
+++ graph_stats.py
@@ -2,16 +2,17 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import math
 
 
 # Graph runtimes
-def graph_runtime(arg):
+def graph_runtime_overall(arg):
     data = pd.read_csv(arg + '/runtimes.csv')
     data = data.fillna('None')
     for graph in data['Input Graph'].unique():
         sub_data = data[data['Input Graph'] == graph]
-        sub_data = sub_data.groupby(['Clusterer Name', 'Config', 'Threads'], dropna=False)['Cluster Time'].mean()
-        sub_data = sub_data.reset_index(level=2)
+        sub_data = sub_data.groupby(['Clusterer Name', 'Threads'], dropna=False)['Cluster Time'].mean()
+        sub_data = sub_data.reset_index(level=1)
         plt.figure(figsize=(12,14))
 
         for index in sorted(sub_data.index.drop_duplicates()):
@@ -24,15 +25,44 @@ def graph_runtime(arg):
         plt.ylabel('Cluster Time (Log Seconds)')
         plt.xlabel('Threads')
         plt.title(graph + ' Clustering Time')
-        plt.savefig(arg + '/' + graph + '_runtime_graph.png')
+        plt.savefig(arg + '/' + graph + '_' + arg + '_runtime_graph.png')
 
-def graph_fscore(arg):
-   # Graph fscore
+
+# Graph runtimes for each clusterer
+def graph_runtime_individual(arg):
+    data = pd.read_csv(arg + '/stats.csv')
+    data = data.fillna('None')
+    for graph in data['Input Graph'].unique():
+        sub_data = data[data['Input Graph'] == graph]
+        for clusterer in data['Clusterer Name'].unique():
+            clusterer_data = sub_data[sub_data['Clusterer Name'] == clusterer]
+            clusterer_data = clusterer_data.groupby(['Config', 'Threads'], dropna=False)['Cluster Time'].mean()
+            clusterer_data = clusterer_data.reset_index(1)
+            #clusterer_data = clusterer_data.sort_values('Cluster Time')
+            plt.figure(figsize=(12,14))
+
+            for index in sorted(clusterer_data.index.drop_duplicates()):
+                plt.plot(clusterer_data.loc[index, 'Threads'], pd.Series(clusterer_data.loc[index, 'Cluster Time']).apply(np.log), label = index, marker = 'o')
+                # sub_data.loc[index].apply(lambda row: plt.text(row['communityRecall_mean'], row['communityPrecision_mean'] * (1 + 0.01) , row['Config'], fontsize=12), axis =1)
+            ax = plt.subplot(111)
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0 + box.height * 0.5,
+                        box.width, box.height * 0.6])
+            plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15))
+            plt.ylabel('Cluster Time (Log Seconds)')
+            plt.xlabel('Threads')
+            plt.title(clusterer + ' ' + graph + ' Clustering Time')
+            plt.savefig(arg + '/' + graph + '_' + arg + '_' + clusterer + '_runtime_graph.png')
+
+# Graph fscore
+def graph_fscore_overall(arg):
     data = pd.read_csv(arg + '/stats.csv')
     data = data.fillna('None')
     for graph in data['Input Graph'].unique():
         sub_data = data[data['Input Graph'] == graph]
         sub_data = sub_data.groupby(['Clusterer Name', 'Config', 'Threads'], dropna=False)[['Cluster Time', 'fScore_mean', 'fScoreParam']].mean()
+        sub_data = sub_data.sort_values('Cluster Time')
+        sub_data =  sub_data.reset_index((1,2))
         plt.figure(figsize=(12,14))
 
         for index in sorted(sub_data.index.drop_duplicates()):
@@ -45,22 +75,78 @@ def graph_fscore(arg):
         plt.ylabel('F Score (param : ' + str(sub_data.iloc[0]['fScoreParam']) + ')')
         plt.xlabel('Cluster Time (Log Seconds)')
         plt.title(graph + ' F-Score')
-        plt.savefig(arg + '/' + graph + '_fscore_graph.png')
+        plt.savefig(arg + '/' + graph + '_' + arg + '_fscore_graph.png')
+
+# Graph fscore for each clusterer
+def graph_fscore_individual(arg):
+    data = pd.read_csv(arg + '/stats.csv')
+    data = data.fillna('None')
+    for graph in data['Input Graph'].unique():
+        sub_data = data[data['Input Graph'] == graph]
+        for clusterer in data['Clusterer Name'].unique():
+            clusterer_data = sub_data[sub_data['Clusterer Name'] == clusterer]
+            clusterer_data = clusterer_data.groupby(['Config', 'Threads'], dropna=False)[['Cluster Time', 'fScore_mean', 'fScoreParam']].mean()
+            clusterer_data = clusterer_data.reset_index(-1)
+            #clusterer_data = clusterer_data.sort_values('Cluster Time')
+            plt.figure(figsize=(12,14))
+
+            for index in sorted(clusterer_data.index.drop_duplicates()):
+                plt.plot(pd.Series(clusterer_data.loc[index, 'Cluster Time']).apply(np.log), clusterer_data.loc[index, 'fScore_mean'], label = index, marker = 'o')
+                # sub_data.loc[index].apply(lambda row: plt.text(row['communityRecall_mean'], row['communityPrecision_mean'] * (1 + 0.01) , row['Config'], fontsize=12), axis =1)
+            ax = plt.subplot(111)
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0 + box.height * 0.5,
+                        box.width, box.height * 0.6])
+            plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15))
+            plt.ylabel('F Score (param : ' + str(sub_data.iloc[0]['fScoreParam']) + ')')
+            plt.xlabel('Cluster Time (Log Seconds)')
+            plt.title(clusterer + ' ' + graph + ' F-Score')
+            plt.savefig(arg + '/' + graph + '_' + arg + '_' + clusterer + '_fscore_graph.png')
 
 
-def graph_precision_recall(arg):
-    # Graph precision recall
+
+# Graph precision recall for each clusterer
+def graph_precision_recall_individual(arg):
+    data = pd.read_csv(arg + '/stats.csv')
+    data = data.fillna('None')
+    for graph in data['Input Graph'].unique():
+        sub_data = data[data['Input Graph'] == graph]
+        for clusterer in data['Clusterer Name'].unique():
+            clusterer_data = sub_data[sub_data['Clusterer Name'] == clusterer]
+            clusterer_data = clusterer_data.groupby(['Config', 'Threads'], dropna=False)[['communityRecall_mean', 'communityPrecision_mean']].mean()
+            clusterer_data = clusterer_data.reset_index(-1)
+            clusterer_data = clusterer_data.sort_values('communityRecall_mean')
+            plt.figure(figsize=(12,14))
+
+            for index in sorted(clusterer_data.index.drop_duplicates()):
+                plt.plot(clusterer_data.loc[index, 'communityRecall_mean'], clusterer_data.loc[index, 'communityPrecision_mean'], label = index, marker = 'o')
+                # sub_data.loc[index].apply(lambda row: plt.text(row['communityRecall_mean'], row['communityPrecision_mean'] * (1 + 0.01) , row['Config'], fontsize=12), axis =1)
+            ax = plt.subplot(111)
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0 + box.height * 0.5,
+                        box.width, box.height * 0.6])
+            plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.15))
+            plt.ylabel('Average Precision')
+            plt.xlabel('Average Recall')
+            plt.xlim(0,1)
+            plt.ylim(0,1)
+            plt.title(clusterer + ' ' + graph + ' Precision-Recall')
+            plt.savefig(arg + '/' + graph + '_' +  arg + '_' + clusterer +'_precisionrecall_graph.png')
+
+
+# Graph precision recall
+def graph_precision_recall_overall(arg):
     data = pd.read_csv(arg + '/stats.csv')
     data = data.fillna('None')
     for graph in data['Input Graph'].unique():
         sub_data = data[data['Input Graph'] == graph]
         sub_data = sub_data.groupby(['Clusterer Name', 'Config', 'Threads'], dropna=False)[['communityRecall_mean', 'communityPrecision_mean']].mean()
-        sub_data = sub_data.reset_index(level=1)
+        sub_data = sub_data.reset_index((1,2))
+        sub_data = sub_data.sort_values('communityRecall_mean')
         plt.figure(figsize=(12,14))
 
         for index in sorted(sub_data.index.drop_duplicates()):
-            plt.plot(sub_data.loc[index, 'communityRecall_mean'], sub_data.loc[index, 'communityPrecision_mean'], label = index, marker = 'o', linestyle='None')
-            # sub_data.loc[index].apply(lambda row: plt.text(row['communityRecall_mean'], row['communityPrecision_mean'] * (1 + 0.01) , row['Config'], fontsize=12), axis =1)
+            plt.plot(sub_data.loc[index, 'communityRecall_mean'], sub_data.loc[index, 'communityPrecision_mean'], label = index, marker = 'o')
         ax = plt.subplot(111)
         box = ax.get_position()
         ax.set_position([box.x0, box.y0 + box.height * 0.5,
@@ -71,21 +157,30 @@ def graph_precision_recall(arg):
         plt.xlim(0,1)
         plt.ylim(0,1)
         plt.title(graph + ' Precision-Recall')
-        plt.savefig(arg + '/' + graph + '_precisionrecall_graph.png')
+        plt.savefig(arg + '/' + graph + '_' + arg +'_precisionrecall_graph_overall.png')
 
 def graph_file(arg, config):
     config = open(config,'r')
     config_info = config.readlines()
     for line in config_info:
-        if line.startswith('graph_precision_recall:'):
+        if line.startswith('graph_precision_recall_overall:'):
             if line.split(' ')[-1].strip() == 'true':
-                graph_precision_recall(arg)
-        elif line.startswith('graph_fscore:'):
+                graph_precision_recall_overall(arg)
+        elif line.startswith('graph_precision_recall_individual:'):
             if line.split(' ')[-1].strip() == 'true':
-                graph_fscore(arg)
-        elif line.startswith('graph_runtime:'):
+                graph_precision_recall_individual(arg)
+        elif line.startswith('graph_fscore_overall:'):
             if line.split(' ')[-1].strip() == 'true':
-                graph_runtime(arg)
+                graph_fscore_overall(arg)
+        elif line.startswith('graph_fscore_individual:'):
+            if line.split(' ')[-1].strip() == 'true':
+                graph_fscore_individual(arg)
+        elif line.startswith('graph_runtime_overall:'):
+            if line.split(' ')[-1].strip() == 'true':
+                graph_runtime_overall(arg)
+        elif line.startswith('graph_runtime_individual:'):
+            if line.split(' ')[-1].strip() == 'true':
+                graph_runtime_individual(arg)
   
 
   
