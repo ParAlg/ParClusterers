52c61d29d75d274b6b748949296179db6a936e6b
Shangdi <shangdiy@mit.edu>
2023-10-25 11:32:24 +0000

    add configs

--- generators/parameters.py
+++ generators/parameters.py
@@ -8,3 +8,18 @@ num_terms = 10  # adjust this as needed
 sequence = [a**i for i in range(num_terms)]
 print('; '.join(map(str, sequence)))
 
+
+# WCC
+import numpy as np
+start = np.log10(0.99)
+stop = np.log10(0.7)
+
+exp_values = np.logspace(start, stop, num=10)
+print('; '.join(map(str, exp_values )))
+
+
+#tectonic
+values = []
+for i in range(1, 300, 10):
+  values.append(0.01* i)
+print('; '.join(map(str, values )))
