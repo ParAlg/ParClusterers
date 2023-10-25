c88dd6d8f0c4328bf8ffb48861251afc85014b9b
Shangdi <shangdiy@mit.edu>
2023-10-25 10:55:54 +0000

    update knn graphs to be normalized

--- cluster_nk.py
+++ cluster_nk.py
@@ -324,10 +324,13 @@ def runNetworKit(clusterer, graph, thread, config, out_prefix, runtime_dict):
       print("Wrote result in %f \n" % (end_time - start_time))
   
   print("postprocessing..." + out_filename)
-  with open(out_filename,'r') as f:
-    run_info = f.readlines()
-    for elem in run_info[1:]:
-      if elem.startswith('Cluster Time:'):
-        runtime_dict['Cluster Time'] = elem.split(' ')[-1].strip()
-      if elem.startswith('Num iterations:'):
-        runtime_dict['Num Iterations'] = elem.split(' ')[-1].strip()
+  try:
+    with open(out_filename,'r') as f:
+      run_info = f.readlines()
+      for elem in run_info[1:]:
+        if elem.startswith('Cluster Time:'):
+          runtime_dict['Cluster Time'] = elem.split(' ')[-1].strip()
+        if elem.startswith('Num iterations:'):
+          runtime_dict['Num Iterations'] = elem.split(' ')[-1].strip()
+  except Exception as e:
+    print(f"An error occurred: {e}")
--- converter/knn_generate.py
+++ converter/knn_generate.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import sys
 from sklearn.neighbors import kneighbors_graph
+from sklearn import datasets, metrics, svm
+from sklearn.datasets import fetch_olivetti_faces
+from numpy.random import RandomState
 import os
 import glob
 import cv2
@@ -10,40 +13,57 @@ def convert_to_weighted(input, config):
     config_info = config.readlines()
     col = -1
     remove = []
-    faces = False
     for line in config_info:
         if line.startswith('label'):
             col = int(line.split(' ')[-1].strip())
         elif line.startswith('remove'):
            for elem in line.split(' ')[-1].strip().split(';'):
               remove.append(int(elem))
-        elif line.startswith('faces'):
-           if line.split(' ')[-1].strip() == 'true':
-              faces = True
     data = pd.DataFrame()
 
-    if not faces:
-      data = pd.read_csv(input, header = None)
-    else:
-      data = []
-      image_index = 0
-      for folder_name in os.listdir(input):
-          folder_path = os.path.join(input, folder_name)
-          pgm_files = glob.glob(os.path.join(folder_path, '*.pgm'))
-          for pgm_file in pgm_files:
-              if not (pgm_file.split('.')[-2].endswith('2') or pgm_file.split('.')[-2].endswith('4')):
-                img = cv2.imread(pgm_file, cv2.IMREAD_GRAYSCALE)
-                img_data = img.flatten()  # Flatten the image data into a 1D array
-                img_data = img_data.tolist()
-                data.append(img_data + [image_index])
-          image_index += 1
+    print(remove)
+    faces = None
+    digits = None
+    faces_targets = None
+
+    if input == "digits":
+      digits = datasets.load_digits()
+      # flatten the images
+      n_samples = len(digits.images)
+      data = digits.images.reshape((n_samples, -1))
       data = pd.DataFrame(data)
-      col = -1
+    elif input == "faces":
+      # random number used for data shuffling
+      rng = RandomState(0)
+      faces, faces_targets = fetch_olivetti_faces(return_X_y=True, shuffle=True, random_state=rng)
+      n_samples, n_features = faces.shape
+
+      # Global centering (focus on one feature, centering all samples)
+      faces_centered = faces - faces.mean(axis=0)
+
+      # Local centering (focus on one sample, centering all features)
+      faces_centered -= faces_centered.mean(axis=1).reshape(n_samples, -1)
+      data = faces_centered
+      data = pd.DataFrame(data)
+
+    else:
+      data = pd.read_csv(input, header = None)
+      print("ground truth col: ", col)
+      print(data.head()[col])
+
       
-      
-    sub_data = data.drop(data.columns[[col]+remove], axis =1)
+    if input in ["digits", "faces"]:
+       sub_data = data
+    else:
+      sub_data = data.drop(data.columns[[col]+remove], axis =1)
+    print("data head: ")
+    print(sub_data.head())
 
     res = kneighbors_graph(sub_data, 10, mode='distance', include_self='auto')
+    max_similarity = res.max()
+    # Divide each non-zero similarity in the matrix by this maximum value.
+    res /= max_similarity
+
     name = input.split('/')[-1].split('.')[0]
     file = open(name + '.graph.txt','w')
     file.write(
@@ -59,7 +79,12 @@ def convert_to_weighted(input, config):
     file.close()
 
     file = open(name + '.cmty.txt','w')
-    result = data.reset_index().groupby(data[data.columns[col]])['index'].apply(list).tolist()
+    if input == "digits":
+      result = data.reset_index().groupby(digits.target)['index'].apply(list).tolist()
+    elif input == "faces":
+      result = data.reset_index().groupby(faces_targets)['index'].apply(list).tolist()
+    else:
+      result = data.reset_index().groupby(data[data.columns[col]])['index'].apply(list).tolist()
     if not (result is None):
       for cluster_list in result:
         file.write("\t".join(str(x) for x in cluster_list) + "\n")
